90c9ba134c0645ad57ca9d744d78052a51a2e784
Hewlbern <michael.t.holborn@gmail.com>
2023-10-11 20:45:53 +1100

    some changes locally

--- brain_translator_model.py
+++ brain_translator_model.py
@@ -1,7 +1,8 @@
+from transformers import BartTokenizer, BartForConditionalGeneration
 import torch.nn as nn
 import torch.nn.functional as F
-from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
 
+# Define the BrainTranslator model
 class BrainTranslator(nn.Module):
     def __init__(self, pretrained_layers, in_feature=840, decoder_embedding_size=1024, additional_encoder_nhead=8, additional_encoder_dim_feedforward=2048):
         super(BrainTranslator, self).__init__()
@@ -11,11 +12,8 @@ class BrainTranslator(nn.Module):
         self.additional_encoder = nn.TransformerEncoder(self.additional_encoder_layer, num_layers=6)
         self.fc1 = nn.Linear(in_feature, decoder_embedding_size)
 
-    def forward(self, input_embeddings_batch, input_masks_batch, input_masks_invert):
-        encoded_embedding = self.additional_encoder(input_embeddings_batch, src_key_padding_mask=input_masks_invert)
+    def forward(self, input_embeddings_batch, input_masks_batch, input_mask_invert, decoder_input_ids):
+        encoded_embedding = self.additional_encoder(input_embeddings_batch, src_key_padding_mask=input_mask_invert)
         encoded_embedding = F.relu(self.fc1(encoded_embedding))
-        
-        # Note: During inference, we don't provide labels. So, it will return logits.
-        out = self.pretrained(inputs_embeds=encoded_embedding, attention_mask=input_masks_batch, return_dict=True)
-        
+        out = self.pretrained(inputs_embeds=encoded_embedding, attention_mask=input_masks_batch, decoder_input_ids=decoder_input_ids, return_dict=True)
         return out
